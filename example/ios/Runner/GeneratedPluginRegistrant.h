21aff28a5f6ef7fb72ddca22eadb7a276055a159
Kate Lovett <katelovett@google.com>
2020-10-12 12:11:28 -0500

    Unrelated changes

--- example/ios/Runner/GeneratedPluginRegistrant.h
+++ example/ios/Runner/GeneratedPluginRegistrant.h
@@ -7,11 +7,8 @@
 
 #import <Flutter/Flutter.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface GeneratedPluginRegistrant : NSObject
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
 @end
 
-NS_ASSUME_NONNULL_END
 #endif /* GeneratedPluginRegistrant_h */
